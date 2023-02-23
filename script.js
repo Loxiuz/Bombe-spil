513f362d24122c71856a67b39f7e88808e7fea16
Malte <maltebjoerklund@gmail.com>
2023-02-23 15:04:37 +0100

    Level Complete and Game Over
    Added incomplete functions gameOver and levelComplete.

--- script.js
+++ script.js
@@ -7,34 +7,35 @@ let points = 0;
 
 function start() {
   console.log("start");
+
+  document.querySelector("#greenButton1_container").classList.add("rising");
+  document.querySelector("#redButton1_container").classList.add("rising");
+
   document
     .querySelector("#greenButton1_container")
     .addEventListener("click", greenbuttonClick);
   document
-    .querySelector("#greenButton1_container")
-    .addEventListener("animationend", greenButtonReset);
-  document
     .querySelector("#redButton1_container")
     .addEventListener("click", redbuttonClick);
-  document
-    .querySelector("#redButton1_container")
-    .addEventListener("animationend", redButtonReset);
 }
 
-function greenbuttonClick() {
-  console.log("greenbuttonClick");
-  document.querySelector("#greenButton1_container").classList.add("paused");
-  document.querySelector("#greenButton1_sprite").classList.add("fade_out");
-  addPoint();
+function gameOver() {
+  console.log("Game Over");
+  removeAnimations();
+  document.querySelector("#game_over").classList.remove("hidden");
 }
-function greenButtonReset() {
-  console.log("greenButtonReset");
-  document.querySelector("#greenButton1_container").classList.remove("paused");
-  document.querySelector("#greenButton1_sprite").classList.remove("fade_out");
+function levelComplete() {
+  console.log("Level Complete");
+  removeAnimations();
+  document.querySelector("#level_complete").classList.remove("hidden");
+}
+
+function removeAnimations() {
+  console.log("Removed animations and event listeners");
   document.querySelector("#greenButton1_container").classList.remove("rising");
-  document.querySelector("#greenButton1_container").offsetHeight;
-  document.querySelector("#greenButton1_container").classList.add("rising");
+  document.querySelector("#redButton1_container").classList.remove("rising");
 }
+
 function addPoint() {
   console.log("addPoint");
   points++;
@@ -45,20 +46,6 @@ function updatePoints() {
   document.querySelector("#counter").textContent = points;
 }
 
-function redbuttonClick() {
-  console.log("redbutton1PauseAndFadeOut");
-  document.querySelector("#redButton1_container").classList.add("paused");
-  document.querySelector("#redButton1_sprite").classList.add("fade_out");
-  lightNextIndicator();
-}
-function redButtonReset() {
-  console.log("redButton1Reset");
-  document.querySelector("#redButton1_container").classList.remove("paused");
-  document.querySelector("#redButton1_sprite").classList.remove("fade_out");
-  document.querySelector("#redButton1_container").classList.remove("rising");
-  document.querySelector("#redButton1_container").offsetHeight;
-  document.querySelector("#redButton1_container").classList.add("rising");
-}
 function lightNextIndicator() {
   console.log("lightNextIndicator");
   updateIndicators();
@@ -69,4 +56,52 @@ function updateIndicators() {
   document
     .querySelector("#indicator_red" + lightsOFF)
     .classList.add("brighter");
+}
+
+function greenbuttonClick() {
+  console.log("greenbuttonClick");
+  document
+    .querySelector("#greenButton1_container")
+    .removeEventListener("click", greenbuttonClick);
+  document.querySelector("#greenButton1_container").classList.add("paused");
+  document.querySelector("#greenButton1_sprite").classList.add("fade_out");
+  document
+    .querySelector("#greenButton1_container")
+    .addEventListener("animationend", greenButtonReset);
+  addPoint();
+}
+function greenButtonReset() {
+  console.log("greenButtonReset");
+  document.querySelector("#greenButton1_container").classList.remove("paused");
+  document.querySelector("#greenButton1_sprite").classList.remove("fade_out");
+  document.querySelector("#greenButton1_container").classList.remove("rising");
+  document.querySelector("#greenButton1_container").offsetHeight;
+  document.querySelector("#greenButton1_container").classList.add("rising");
+  document
+    .querySelector("#greenButton1_container")
+    .addEventListener("click", greenbuttonClick);
+}
+
+function redbuttonClick() {
+  console.log("redbutton1PauseAndFadeOut");
+  document
+    .querySelector("#redButton1_container")
+    .removeEventListener("click", redbuttonClick);
+  document.querySelector("#redButton1_container").classList.add("paused");
+  document.querySelector("#redButton1_sprite").classList.add("fade_out");
+  document
+    .querySelector("#redButton1_container")
+    .addEventListener("animationend", redButtonReset);
+  lightNextIndicator();
+}
+function redButtonReset() {
+  console.log("redButton1Reset");
+  document.querySelector("#redButton1_container").classList.remove("paused");
+  document.querySelector("#redButton1_sprite").classList.remove("fade_out");
+  document.querySelector("#redButton1_container").classList.remove("rising");
+  document.querySelector("#redButton1_container").offsetHeight;
+  document.querySelector("#redButton1_container").classList.add("rising");
+  document
+    .querySelector("#redButton1_container")
+    .addEventListener("click", redbuttonClick);
 }
