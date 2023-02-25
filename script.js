93eb19ad1a719f24834a9f7c821fa2332e874cd4
Malte <maltebjoerklund@gmail.com>
2023-02-25 21:38:42 +0100

    Funktioner til alle knapper

--- script.js
+++ script.js
@@ -9,14 +9,30 @@ function start() {
   console.log("start");
 
   document.querySelector("#greenButton1_container").classList.add("rising");
+  document.querySelector("#greenButton2_container").classList.add("rising");
+  document.querySelector("#greenButton3_container").classList.add("rising");
   document.querySelector("#redButton1_container").classList.add("rising");
+  document.querySelector("#redButton2_container").classList.add("rising");
+  document.querySelector("#redButton3_container").classList.add("rising");
 
   document
     .querySelector("#greenButton1_container")
-    .addEventListener("click", greenButtonClick);
+    .addEventListener("click", greenButton1Click);
+  document
+    .querySelector("#greenButton2_container")
+    .addEventListener("click", greenButton2Click);
+  document
+    .querySelector("#greenButton3_container")
+    .addEventListener("click", greenButton3Click);
   document
     .querySelector("#redButton1_container")
-    .addEventListener("click", redButtonClick);
+    .addEventListener("click", redButton1Click);
+  document
+    .querySelector("#redButton2_container")
+    .addEventListener("click", redButton2Click);
+  document
+    .querySelector("#redButton3_container")
+    .addEventListener("click", redButton3Click);
 }
 
 function gameOver() {
@@ -34,19 +50,49 @@ function removeAnimations() {
   //Removes animations and hides game elements and game ui.
   console.log("Removed animations and event listeners");
   document.querySelector("#greenButton1_container").classList.remove("rising");
+  document.querySelector("#greenButton2_container").classList.remove("rising");
+  document.querySelector("#greenButton3_container").classList.remove("rising");
   document.querySelector("#redButton1_container").classList.remove("rising");
+  document.querySelector("#redButton2_container").classList.remove("rising");
+  document.querySelector("#redButton3_container").classList.remove("rising");
   document
     .querySelector("#greenButton1_container")
-    .removeEventListener("click", greenButtonClick);
-  document
-    .querySelector("#redButton1_container")
-    .removeEventListener("click", redButtonClick);
+    .removeEventListener("click", greenButton1Click);
   document
     .querySelector("#greenButton1_container")
-    .removeEventListener("animationend", greenButtonReset);
+    .removeEventListener("animationend", greenButton1Reset);
+  document;
+  document
+    .querySelector("#greenButton2_container")
+    .removeEventListener("click", greenButton2Click);
+  document
+    .querySelector("#greenButton2_container")
+    .removeEventListener("animationend", greenButton2Reset);
+  document;
+  document
+    .querySelector("#greenButton3_container")
+    .removeEventListener("click", greenButton3Click);
+  document
+    .querySelector("#greenButton3_container")
+    .removeEventListener("animationend", greenButton3Reset);
   document
     .querySelector("#redButton1_container")
-    .removeEventListener("animationend", redButtonReset);
+    .removeEventListener("click", redButton1Click);
+  document
+    .querySelector("#redButton1_container")
+    .removeEventListener("animationend", redButton1Reset);
+  document
+    .querySelector("#redButton2_container")
+    .removeEventListener("click", redButton2Click);
+  document
+    .querySelector("#redButton2_container")
+    .removeEventListener("animationend", redButton2Reset);
+  document
+    .querySelector("#redButton3_container")
+    .removeEventListener("click", redButton3Click);
+  document
+    .querySelector("#redButton3_container")
+    .removeEventListener("animationend", redButton3Reset);
 
   document.querySelector("#game_elements").classList.add("hidden");
   document.querySelector("#game_ui").classList.add("hidden");
@@ -80,20 +126,20 @@ function updateIndicators() {
     .classList.add("brighter");
 }
 
-function greenButtonClick() {
-  console.log("greenbuttonClick");
+function greenButton1Click() {
+  console.log("greenbutton1Click");
   document
     .querySelector("#greenButton1_container")
-    .removeEventListener("click", greenButtonClick);
+    .removeEventListener("click", greenButton1Click);
   document.querySelector("#greenButton1_container").classList.add("paused");
   document.querySelector("#greenButton1_sprite").classList.add("fade_out");
   document
     .querySelector("#greenButton1_container")
-    .addEventListener("animationend", greenButtonReset);
+    .addEventListener("animationend", greenButton1Reset);
   addPoint();
 }
-function greenButtonReset() {
-  console.log("greenButtonReset");
+function greenButton1Reset() {
+  console.log("greenButton1Reset");
   document.querySelector("#greenButton1_container").classList.remove("paused");
   document.querySelector("#greenButton1_sprite").classList.remove("fade_out");
   document.querySelector("#greenButton1_container").classList.remove("rising");
@@ -101,22 +147,70 @@ function greenButtonReset() {
   document.querySelector("#greenButton1_container").classList.add("rising");
   document
     .querySelector("#greenButton1_container")
-    .addEventListener("click", greenButtonClick);
+    .addEventListener("click", greenButton1Click);
 }
 
-function redButtonClick() {
-  console.log("redbutton1PauseAndFadeOut");
+function greenButton2Click() {
+  console.log("greenbutton2Click");
+  document
+    .querySelector("#greenButton2_container")
+    .removeEventListener("click", greenButton2Click);
+  document.querySelector("#greenButton2_container").classList.add("paused");
+  document.querySelector("#greenButton2_sprite").classList.add("fade_out");
+  document
+    .querySelector("#greenButton2_container")
+    .addEventListener("animationend", greenButton2Reset);
+  addPoint();
+}
+function greenButton2Reset() {
+  console.log("greenButton2Reset");
+  document.querySelector("#greenButton2_container").classList.remove("paused");
+  document.querySelector("#greenButton2_sprite").classList.remove("fade_out");
+  document.querySelector("#greenButton2_container").classList.remove("rising");
+  document.querySelector("#greenButton2_container").offsetHeight;
+  document.querySelector("#greenButton2_container").classList.add("rising");
+  document
+    .querySelector("#greenButton2_container")
+    .addEventListener("click", greenButton2Click);
+}
+
+function greenButton3Click() {
+  console.log("greenbutton3Click");
+  document
+    .querySelector("#greenButton3_container")
+    .removeEventListener("click", greenButton3Click);
+  document.querySelector("#greenButton3_container").classList.add("paused");
+  document.querySelector("#greenButton3_sprite").classList.add("fade_out");
+  document
+    .querySelector("#greenButton3_container")
+    .addEventListener("animationend", greenButton3Reset);
+  addPoint();
+}
+function greenButton3Reset() {
+  console.log("greenButton3Reset");
+  document.querySelector("#greenButton3_container").classList.remove("paused");
+  document.querySelector("#greenButton3_sprite").classList.remove("fade_out");
+  document.querySelector("#greenButton3_container").classList.remove("rising");
+  document.querySelector("#greenButton3_container").offsetHeight;
+  document.querySelector("#greenButton3_container").classList.add("rising");
+  document
+    .querySelector("#greenButton3_container")
+    .addEventListener("click", greenButton3Click);
+}
+
+function redButton1Click() {
+  console.log("redbutton1Click");
   document
     .querySelector("#redButton1_container")
-    .removeEventListener("click", redButtonClick);
+    .removeEventListener("click", redButton1Click);
   document.querySelector("#redButton1_container").classList.add("paused");
   document.querySelector("#redButton1_sprite").classList.add("fade_out");
   document
     .querySelector("#redButton1_container")
-    .addEventListener("animationend", redButtonReset);
+    .addEventListener("animationend", redButton1Reset);
   lightNextIndicator();
 }
-function redButtonReset() {
+function redButton1Reset() {
   console.log("redButton1Reset");
   document.querySelector("#redButton1_container").classList.remove("paused");
   document.querySelector("#redButton1_sprite").classList.remove("fade_out");
@@ -125,5 +219,51 @@ function redButtonReset() {
   document.querySelector("#redButton1_container").classList.add("rising");
   document
     .querySelector("#redButton1_container")
-    .addEventListener("click", redButtonClick);
+    .addEventListener("click", redButton1Click);
+}
+function redButton2Click() {
+  console.log("redbutton2Click");
+  document
+    .querySelector("#redButton2_container")
+    .removeEventListener("click", redButton2Click);
+  document.querySelector("#redButton2_container").classList.add("paused");
+  document.querySelector("#redButton2_sprite").classList.add("fade_out");
+  document
+    .querySelector("#redButton2_container")
+    .addEventListener("animationend", redButton2Reset);
+  lightNextIndicator();
+}
+function redButton2Reset() {
+  console.log("redButton2Reset");
+  document.querySelector("#redButton2_container").classList.remove("paused");
+  document.querySelector("#redButton2_sprite").classList.remove("fade_out");
+  document.querySelector("#redButton2_container").classList.remove("rising");
+  document.querySelector("#redButton2_container").offsetHeight;
+  document.querySelector("#redButton2_container").classList.add("rising");
+  document
+    .querySelector("#redButton2_container")
+    .addEventListener("click", redButton2Click);
+}
+function redButton3Click() {
+  console.log("redbutton3Click");
+  document
+    .querySelector("#redButton3_container")
+    .removeEventListener("click", redButton3Click);
+  document.querySelector("#redButton3_container").classList.add("paused");
+  document.querySelector("#redButton3_sprite").classList.add("fade_out");
+  document
+    .querySelector("#redButton3_container")
+    .addEventListener("animationend", redButton3Reset);
+  lightNextIndicator();
+}
+function redButton3Reset() {
+  console.log("redButton3Reset");
+  document.querySelector("#redButton3_container").classList.remove("paused");
+  document.querySelector("#redButton3_sprite").classList.remove("fade_out");
+  document.querySelector("#redButton3_container").classList.remove("rising");
+  document.querySelector("#redButton3_container").offsetHeight;
+  document.querySelector("#redButton3_container").classList.add("rising");
+  document
+    .querySelector("#redButton3_container")
+    .addEventListener("click", redButton3Click);
 }
