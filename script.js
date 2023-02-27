2beed7c515b58f8515a7ebba76a4000c6997457e
Malte <maltebjoerklund@gmail.com>
2023-02-27 23:00:10 +0100

    Generaliseret med 'Math.random' og 'this'

--- script.js
+++ script.js
@@ -8,19 +8,76 @@ let points = 0;
 function start() {
   console.log("start");
 
-  document.querySelector("#greenButton1_container").classList.add("rising");
-  document.querySelector("#greenButton2_container").classList.add("rising");
-  document.querySelector("#greenButton3_container").classList.add("rising");
-  document.querySelector("#redButton1_container").classList.add("rising");
-  document.querySelector("#redButton2_container").classList.add("rising");
-  document.querySelector("#redButton3_container").classList.add("rising");
+  startAnimations();
 
+  //Registrering af click
   document
     .querySelector("#greenButton1_container")
     .addEventListener("click", greenButtonClick);
   document
+    .querySelector("#greenButton2_container")
+    .addEventListener("click", greenButtonClick);
+  document
+    .querySelector("#greenButton3_container")
+    .addEventListener("click", greenButtonClick);
+  document
     .querySelector("#redButton1_container")
     .addEventListener("click", redButtonClick);
+  document
+    .querySelector("#redButton2_container")
+    .addEventListener("click", redButtonClick);
+  document
+    .querySelector("#redButton3_container")
+    .addEventListener("click", redButtonClick);
+  //Hvad der skal ske når animationen slutter
+  document
+    .querySelector("#greenButton1_container")
+    .addEventListener("animationiteration", buttonReset);
+  document
+    .querySelector("#greenButton2_container")
+    .addEventListener("animationiteration", buttonReset);
+  document
+    .querySelector("#greenButton3_container")
+    .addEventListener("animationiteration", buttonReset);
+  document
+    .querySelector("#redButton1_container")
+    .addEventListener("animationiteration", buttonReset);
+  document
+    .querySelector("#redButton2_container")
+    .addEventListener("animationiteration", buttonReset);
+  document
+    .querySelector("#redButton3_container")
+    .addEventListener("animationiteration", buttonReset);
+}
+
+function startAnimations() {
+  //Animation
+  document
+    .querySelector("#greenButton1_container")
+    .classList.add("rise_and_fall");
+  document
+    .querySelector("#greenButton2_container")
+    .classList.add("rise_and_fall");
+  document
+    .querySelector("#greenButton3_container")
+    .classList.add("rise_and_fall");
+  document
+    .querySelector("#redButton1_container")
+    .classList.add("rise_and_fall");
+  document
+    .querySelector("#redButton2_container")
+    .classList.add("rise_and_fall");
+  document
+    .querySelector("#redButton3_container")
+    .classList.add("rise_and_fall");
+
+  //Positioner
+  document.querySelector("#greenButton1_container").classList.add("position1");
+  document.querySelector("#greenButton2_container").classList.add("position3");
+  document.querySelector("#greenButton3_container").classList.add("position6");
+  document.querySelector("#redButton1_container").classList.add("position2");
+  document.querySelector("#redButton2_container").classList.add("position4");
+  document.querySelector("#redButton3_container").classList.add("position5");
 }
 
 function gameOver() {
@@ -37,35 +94,101 @@ function levelComplete() {
 //Removes animations and hides game elements and game ui.
 function removeAnimations() {
   console.log("Removed animations and event listeners");
-  document.querySelector("#greenButton1_container").classList.remove("rising");
-  document.querySelector("#greenButton2_container").classList.remove("rising");
-  document.querySelector("#greenButton3_container").classList.remove("rising");
-  document.querySelector("#redButton1_container").classList.remove("rising");
-  document.querySelector("#redButton2_container").classList.remove("rising");
-  document.querySelector("#redButton3_container").classList.remove("rising");
-
   document
     .querySelector("#greenButton1_container")
-    .removeEventListener("click", greenButtonClick);
+    .classList.remove("rise_and_fall");
   document
-    .querySelector("#greenButton1_container")
-    .removeEventListener("animationend", greenButtonReset);
+    .querySelector("#greenButton2_container")
+    .classList.remove("rise_and_fall");
+  document
+    .querySelector("#greenButton3_container")
+    .classList.remove("rise_and_fall");
   document
     .querySelector("#redButton1_container")
-    .removeEventListener("click", redButtonClick);
+    .classList.remove("rise_and_fall");
   document
-    .querySelector("#redButton1_container")
-    .removeEventListener("animationend", redButtonReset);
+    .querySelector("#redButton2_container")
+    .classList.remove("rise_and_fall");
+  document
+    .querySelector("#redButton3_container")
+    .classList.remove("rise_and_fall");
 
   document.querySelector("#game_elements").classList.add("hidden");
   document.querySelector("#game_ui").classList.add("hidden");
+}
+
+function greenButtonClick() {
+  console.log("greenButtonClick");
+  const button = this;
+
+  button.removeEventListener("click", greenButtonClick);
+  button.classList.add("paused");
+  button.querySelector("img").classList.add("fade_out");
+  button.addEventListener("animationend", greenButtonGone);
+  addPoint();
+}
+function greenButtonGone() {
+  console.log("greenButtonGone");
+  const button = this;
+
+  button.removeEventListener("animationend", greenButtonGone);
+  button.querySelector("img").classList.remove("fade_out");
+  button.classList.remove("paused");
+
+  buttonReset.call(this);
+
+  button.addEventListener("click", greenButtonClick);
+}
+
+function redButtonClick() {
+  console.log("redbuttonClick");
+  const button = this;
+
+  button.removeEventListener("click", redButtonClick);
+  button.classList.add("paused");
+  button.querySelector("img").classList.add("fade_out");
+  button.addEventListener("animationend", redButtonGone);
+  lightNextIndicator();
+}
+function redButtonGone() {
+  console.log("redButtonGone");
+  const button = this;
+
+  button.removeEventListener("animationend", redButtonGone);
+  button.querySelector("img").classList.remove("fade_out");
+  button.classList.remove("paused");
+
+  buttonReset.call(this);
+
+  button.addEventListener("click", redButtonClick);
+}
+
+function buttonReset() {
+  console.log("buttonReset");
+  const button = this;
+
+  button.classList.remove("rise_and_fall");
+  button.offsetHeight;
+  button.classList.add("rise_and_fall");
+
+  button.classList.remove(
+    "position1",
+    "position2",
+    "position3",
+    "position4",
+    "position5",
+    "position6"
+  );
+
+  const n = Math.ceil(Math.random() * 6);
+  button.classList.add("position" + n);
 }
 
 function addPoint() {
   console.log("addPoint");
   points++;
   updatePoints();
-  if (points == 10) {
+  if (points == 3) {
     levelComplete();
   }
 }
@@ -78,61 +201,17 @@ function lightNextIndicator() {
   console.log("lightNextIndicator");
   updateIndicators();
   indicatorsOff--;
-  if (indicatorsOff == 0) {
+  //Så man har sidste forsøg når alle indikatorer er lyst op
+  if (indicatorsOff < 0) {
     gameOver();
   }
 }
 function updateIndicators() {
   console.log("updateIndicators");
-  document
-    .querySelector("#indicator_red" + indicatorsOff)
-    .classList.add("brighter");
-}
-
-function greenButtonClick() {
-  console.log("greenbuttonClick");
-  document
-    .querySelector("#greenButton1_container")
-    .removeEventListener("click", greenButtonClick);
-  document.querySelector("#greenButton1_container").classList.add("paused");
-  document.querySelector("#greenButton1_sprite").classList.add("fade_out");
-  document
-    .querySelector("#greenButton1_container")
-    .addEventListener("animationend", greenButtonReset);
-  addPoint();
-}
-function greenButtonReset() {
-  console.log("greenButtonReset");
-  document.querySelector("#greenButton1_container").classList.remove("paused");
-  document.querySelector("#greenButton1_sprite").classList.remove("fade_out");
-  document.querySelector("#greenButton1_container").classList.remove("rising");
-  document.querySelector("#greenButton1_container").offsetHeight;
-  document.querySelector("#greenButton1_container").classList.add("rising");
-  document
-    .querySelector("#greenButton1_container")
-    .addEventListener("click", greenButtonClick);
-}
-
-function redButtonClick() {
-  console.log("redbuttonClick");
-  document
-    .querySelector("#redButton1_container")
-    .removeEventListener("click", redButtonClick);
-  document.querySelector("#redButton1_container").classList.add("paused");
-  document.querySelector("#redButton1_sprite").classList.add("fade_out");
-  document
-    .querySelector("#redButton1_container")
-    .addEventListener("animationend", redButtonReset);
-  lightNextIndicator();
-}
-function redButtonReset() {
-  console.log("redButtonReset");
-  document.querySelector("#redButton1_container").classList.remove("paused");
-  document.querySelector("#redButton1_sprite").classList.remove("fade_out");
-  document.querySelector("#redButton1_container").classList.remove("rising");
-  document.querySelector("#redButton1_container").offsetHeight;
-  document.querySelector("#redButton1_container").classList.add("rising");
-  document
-    .querySelector("#redButton1_container")
-    .addEventListener("click", redButtonClick);
+  //For at den ikke prøver at indsætte 0 i navnet
+  if (indicatorsOff > 0) {
+    document
+      .querySelector("#indicator_red" + indicatorsOff)
+      .classList.add("brighter");
+  }
 }
